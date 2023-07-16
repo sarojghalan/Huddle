5624e0109c432aa9015a0b46a460fe990b60b9f7
Hypekage <jmestmnag@gmail.com>
2023-07-16 14:37:20 +0545

    Learning Nested Routing
    Nested routing is also so easy , just making another folder inside parent folder helps to create nested folder

---
+++ app/about/team/page.tsx
@@ -0,0 +1,13 @@
+const Team = () => {
+  return (
+    <>
+      <h1>Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+        odit.
+      </p>
+    </>
+  );
+};
+
+export default Team;
