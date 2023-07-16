40a8c1f30d30f75b5ef50d989d371a65b46b7628
Hypekage <jmestmnag@gmail.com>
2023-07-16 14:36:00 +0545

    Learning App Routing
    where in next, routing process has been so easy by making folder you can access routes

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+        asperiores fugiat accusamus aspernatur est sunt sequi illo minima at
+        veniam? Assumenda aut suscipit repudiandae adipisci velit a vero natus
+        cupiditate!
+      </p>
+    </>
+  );
+};
+
+export default About;
