470807854c5ec856e464c58e55c49b8591b09a51
Hypekage <jmestmnag@gmail.com>
2023-07-17 23:04:51 +0545

    Repos card and Info was created
    As moving into next route i have created a new file to show the repo detail and all the required detail

--- app/code/repos/page.tsx
+++ app/code/repos/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode } from "react";
 import { FaCodeBranch, FaStar, FaEye } from "react-icons/fa";
 
 async function fetchRepos() {
   const response = await fetch(
     "https://api.github.com/users/sarojghalan/repos"
   );
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const repos = await response.json();
   return repos;
 }
@@ -13,12 +15,12 @@ const Repos = async () => {
   const repos = await fetchRepos();
   console.log(repos);
   return (
-      <div className="repos-container">
-        <h2>Repositories</h2>
-        <ul className="repo-list">
-          {repos?.map((item) => {
-            return (
-              <li key={item?.id}>
+    <div className="repos-container">
+      <h2>Repositories</h2>
+      <ul className="repo-list">
+        {repos?.map((item: { id: Key | null | undefined; name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; description: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; stargazers_count: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; forks_count: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; watchers_count: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; }) => {
+          return (
+            <li key={item?.id}>
               <Link href={`/code/repos/${item?.name}`}>
                 <h3>{item?.name}</h3>
                 <p>{item?.description}</p>
@@ -35,10 +37,10 @@ const Repos = async () => {
                 </div>
               </Link>
             </li>
-            )
-          })}
-        </ul>
-      </div>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
